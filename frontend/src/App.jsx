52d9945a1ba00a4df1226646df20c5312e290d75
m1ngxuan <mingxuan.zhang1@my.jcu.edu.au>
2025-06-23 22:29:41 +1000

    import basic page and add to router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react';
 import { Routes, Route, BrowserRouter, useLocation } from 'react-router-dom';
 
+// Import page and UI components
+import Home from './components/Homepage';
+import LoginStd from './components/loginPageStd';
+import Signup from './components/registerPage';
+import LoginStf from './components/loginPageStf';
+import CssBaseline from '@mui/material/CssBaseline';
+
 function App() {
   
+  const [token, setToken] = useState(localStorage.getItem('token'));
+
   return (
     <BrowserRouter>
+      <CssBaseline />
       <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="/about" element={<h1>About Page</h1>} />
-        <Route path="/contact" element={<h1>Contact Page</h1>} />
+        <Route path="/" element={<LoginStd setToken={setToken}/>} />
+        <Route path="/index" element={<Home token={token}/>} />
+        <Route path="/student" element={<LoginStd setToken={setToken} />} />
+        <Route path="/staff" element={<LoginStf setToken={setToken} />} />
+        <Route path="/signup" element={<Signup setToken={setToken} />} />
       </Routes>
     </BrowserRouter>
   );
