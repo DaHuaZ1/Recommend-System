375ee26b569705548c4a538719c8ca29bea56b89
m1ngxuan <mingxuan.zhang1@my.jcu.edu.au>
2025-07-08 02:01:32 +1000

    add basic page

---
+++ frontend/src/components/groupPageStd.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TopBar from './Bar';
+
+export default function GroupStd() {
+  return (
+    <div className="container">
+        <TopBar />
+      <h1>Group Page</h1>
+      <p>This is the group page for students.</p>
+      {/* Add more group-related content here */}
+    </div>
+  );
+}
---
+++ frontend/src/components/recommendStd.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TopBar from './Bar';
+
+export default function RecommendStd() {
+  return (
+    <div className="container">
+        <TopBar />
+      <h1>Recommend Page</h1>
+      <p>This is the recommend page for students.</p>
+      {/* Add more recommend-related content here */}
+    </div>
+  );
+}
