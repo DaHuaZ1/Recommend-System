0258af680baf78a15abca2cc3c22d4930ac798e6
m1ngxuan <mingxuan.zhang1@my.jcu.edu.au>
2025-07-11 00:52:40 +1000

    remove console.log

--- frontend/src/components/scrollToTop.jsx
+++ frontend/src/components/scrollToTop.jsx
@@ -4,7 +4,6 @@ import { useLocation } from "react-router-dom";
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
-  console.log("ScrollToTop triggered for pathname:", pathname);
   useLayoutEffect(() => {
     // Scroll to top when the pathname changes
     window.scrollTo({
